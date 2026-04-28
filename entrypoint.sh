e5102417c309a3cee1ab8f99e63c8ab38562b9a1
Kosae <qusaiidlbi@gmail.com>
2026-04-28 15:28:49 +0300

    addfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,13 +19,13 @@ done
 echo "Database connected!"
 
 # --- 3. تشغيل المهاجرات (Migrations) ---
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
 
 # --- 4. تشغيل البذور (Seeders) ---
 # أضفنا || true لاستمرار العمل حتى لو حدث خطأ في الـ Seed (لتجنب مشاكل البيانات المكررة)
 echo "Running seeders..."
-# php artisan db:seed --force -v || true
+php artisan db:seed --force -v || true
 
 echo "Starting server..."
 
